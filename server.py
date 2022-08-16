db40b1fe886a898dfe9d84fa172c8b83c23e09eb
Marin <wangwenzeng@tal.com>
2022-08-16 15:46:49 +0800

    use paddlepaddle

--- server.py
+++ server.py
@@ -5,39 +5,32 @@ import json
 import cv2
 import numpy as np
 import sqlite3
+from deploy.python.predict_system import predictSingleImg
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
-define("path", default="/usr/local/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml", help="run on the given port", type=str)
 
 conn=sqlite3.connect("ultraman.db")
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("ultraman.xml")
 class MainHandler(tornado.web.RequestHandler):    
     def post(self):
-        faceCascade = cv2.CascadeClassifier(options.path)
+        # 获取图片数据
         data = json.loads(self.request.body)
-
         img = data["img"]
         imgData = base64.b64decode(img)
-
+        # 读取图片
         img1 = cv2.imdecode(np.array(bytearray(imgData)),cv2.IMREAD_COLOR)
-        gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.15,
-            minNeighbors=5,
-            minSize=(5, 5)
-        )
-        for (x, y, w, h) in faces:
-            tmp_img = gray[y:y+h, x:x+w]
-            label, confidence = recognizer.predict(tmp_img)
-            sql = "SELECT * FROM ultraman WHERE id = %d" % label
-            row = conn.execute(sql).fetchone()
-        
-        newImgData = np.array(cv2.imencode('.jpg',img1)[1])
-
+        # 预测
+        data = predictSingleImg("deploy/configs/inference_ultraman.yaml",img1)
+        name = 'marin'
+        # 获取预测到的图片名称
+        if len(data) > 0:
+            rs = data[0]
+            name = rs['rec_docs']        
+        # 数据查询
+        sql = "SELECT * FROM ultraman WHERE name = '%s'" % name
+        row = conn.execute(sql).fetchone()
+        # 返回
         resp = dict()
         # resp["img"] = str(base64.b64encode(newImgData),'utf-8')
         # resp["name"] = "迪迦奥特曼"
